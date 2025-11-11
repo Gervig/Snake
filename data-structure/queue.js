2223f2774724587f1e07bdc3cc4d68a923bbdf7f
Gervig <gervig91@gmail.com>
2025-11-11 13:16:14 +0100

    queue & linked list

--- data-structure/queue.js
+++ data-structure/queue.js
@@ -1,3 +1,38 @@
-export default class Queue{
+import DoublyLinkedList from "./doublylinkedlist";
 
+export default class Queue {
+  constructor() {
+    this.list = new DoublyLinkedList();
+  }
+
+  head() {
+    return this.list.head;
+  }
+
+  tail() {
+    return this.list.tail;
+  }
+
+  enqueue(data) {
+    this.list.addLast(data);
+    return this.tail();
+  }
+
+  dequeue() {
+    const node = this.head();
+    this.list.removeFirst();
+    return node;
+  }
+
+  peek() {
+    return this.head();
+  }
+
+  size() {
+    return this.list._size;
+  }
+
+  get(index) {
+    return this.list.get(index);
+  }
 }
